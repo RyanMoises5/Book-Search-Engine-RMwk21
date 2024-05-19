74b8f0f3e747eb6d119f0ac93de7d9d7a4ef0444
Ryan M <ryan.a.moises@gmail.com>
2024-05-19 00:39:32 -0400

    Graphql configuured and functioning

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,32 @@
+const typeDefs = `
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+  }
+
+  type Book {
+    _id: ID!
+    authors: [String]!
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Query {
+    user(userId: ID!): User
+  }
+`;
+
+  // type Mutation {
+  //   addUser(): Auth
+  //   login(): Auth
+  //   saveBook(): 
+  //   deleteBook():
+  // }
+
+module.exports = typeDefs
